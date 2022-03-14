1fa453bc83d44e5e46d694b28006ef75cba10b8c
Tien Do Xuan <intern.dxtien@tma.com.vn>
2022-03-14 17:28:59 +0700

    update

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -5,14 +5,32 @@ class Counters extends Component {
     state = {  
 
         counters:[
-            {id:1, value:0},
-            
+            {id:1, value:3},
+            {id:2, value:2},
+                     
         ]
-    } 
+    };
+
+    handleDelete = counterId => {
+        
+        const counters = this.state.counter.filter(c =>c.id !== counterId);
+        this.setState({counters});
+
+    };
     render() { 
+        
         return (
             <> 
-                {this.state.counters.map(counter => <Counter key= {counter.id} />)}
+                {this.state.counters.map(counter => (
+                <Counter 
+                key= {counter.id}  
+                onDelete={this.handleDelete} 
+                value ={counter.value}
+                >
+
+                </Counter>    
+                ))}
+                
             </>
         )
     }
